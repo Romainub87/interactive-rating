5edd3151d673914f8b111cef4928c8f9686ba48e
Romainub87 <106332313+Romainub87@users.noreply.github.com>
2023-01-21 22:03:53 +0100

    refactor

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 let $encadreThank = $(".encadre-thank");
 let $encadre = $(".encadre");
-let $rate = $('.rate');
+let $rate = $(".rate");
 let $alert = $(".alert");
 let $chiffre = $(".chiffre");
 
-
-// Animation hover notes 
-$(".chiffre").hover(function () {
-        // over
-        $(this).css({
-            "background-color": "hsl(25, 97%, 53%)",
-            "color": "white",
-            "cursor": "pointer",
-        });
-        $(this).prev(".chiffre").css({
-            "background-color": "hsl(216, 12%, 54%)",
-            "color": "white",
-        });
-        $(this).next(".chiffre").css({
-            "background-color": "hsl(216, 12%, 54%)",
-            "color": "white",
-        });
-        
-    }, function () {
-        // out
-        $(this).css({
-            "background-color": "hsl(212, 21%, 24%)",
-            "color": "hsl(216, 12%, 54%)",
-        });
-        $(this).prev(".chiffre").css({
-            "background-color": "hsl(212, 21%, 24%)",
-            "color": "hsl(216, 12%, 54%)",
-        });
-        $(this).next(".chiffre").css({
-            "background-color": "hsl(212, 21%, 24%)",
-            "color": "hsl(216, 12%, 54%)",
-        });
-    }
+// Animation hover notes
+$(".chiffre").hover(
+  function () {
+    // over
+    $(this).css({
+      "background-color": "hsl(25, 97%, 53%)",
+      color: "white",
+      cursor: "pointer",
+    });
+    $(this).prev(".chiffre").css({
+      "background-color": "hsl(216, 12%, 54%)",
+      color: "white",
+    });
+    $(this).next(".chiffre").css({
+      "background-color": "hsl(216, 12%, 54%)",
+      color: "white",
+    });
+  },
+  function () {
+    // out
+    $(this).css({
+      "background-color": "hsl(212, 21%, 24%)",
+      color: "hsl(216, 12%, 54%)",
+    });
+    $(this).prev(".chiffre").css({
+      "background-color": "hsl(212, 21%, 24%)",
+      color: "hsl(216, 12%, 54%)",
+    });
+    $(this).next(".chiffre").css({
+      "background-color": "hsl(212, 21%, 24%)",
+      color: "hsl(216, 12%, 54%)",
+    });
+  }
 );
 
 // Etat initial
@@ -46,18 +46,16 @@ $encadreThank.hide();
 // Choix de la note
 let valueRate = 0;
 $chiffre.on("click", function () {
-    valueRate = $(this).html();
+  valueRate = $(this).html();
 });
 
 // Affichage encadre-thank
 $(".submit").on("click", function () {
-    if (valueRate != 0) {
-        $encadreThank.toggle();
-        $encadre.toggle();
-        $rate.text(valueRate);
-    } else {
-        $alert.show();
-    }
-   
+  if (valueRate != 0) {
+    $encadreThank.toggle();
+    $encadre.toggle();
+    $rate.text(valueRate);
+  } else {
+    $alert.show();
+  }
 });
-
